bac2c73f1d308f04298d0f8c3be4a195212861d4
smallpp420 <78730004+SmallPP420@users.noreply.github.com>
2022-02-28 17:15:33 +0000

    Fix Linux Test CI (#3)

--- TestBuild.sh
+++ TestBuild.sh
@@ -2,3 +2,6 @@
 # Made by SmallPP420
 # the size of this program is gonna be huge xd
 dotnet publish "/home/runner/work/Meme-Downloader-2022/Meme-Downloader-2022/Meme Downloader 2022.csproj" --output "build\\" --arch x64 --os linux -c release --self-contained true # linux build
+cd /home/runner/work/Meme-Downloader-2022/Meme-Downloader-2022/build/
+chmod +x "Meme Downloader 2022"
+./"Meme Downloader 2022" -ci
